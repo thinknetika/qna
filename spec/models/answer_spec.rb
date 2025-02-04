ba5da9a70e561a094edb2e5da8b5ad78eeca024c
elbub1979 <ilya.leuh09@gmail.com>
2025-02-04 18:25:07 +0300

    added question, answers models with validations, associations; added test for question, answer models

---
+++ spec/models/answer_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe Answer, type: :model do
+  describe "validations" do
+    context "body" do
+      it { should validate_presence_of(:body) }
+    end
+  end
+
+  describe "associations" do
+    context "answers" do
+      it { should belong_to(:question) }
+    end
+  end
+end
