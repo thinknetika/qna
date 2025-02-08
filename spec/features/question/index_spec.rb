a9f056f8fac57d4edfaa5c41d44c8253caf55b30
elbub1979 <ilya.leuh09@gmail.com>
2025-02-09 01:21:49 +0300

    added index questions acceptance test

---
+++ spec/features/question/index_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+feature 'User can view all the questions', %q(
+  To get information or find answers
+  As an authenticated or unauthenticated user
+  I would like to be able to view all the questions.
+) do
+  given(:user) { create(:user) }
+  given!(:questions) { create_list(:question, 3) }
+
+  scenario 'Unauthenticated user can view all questions' do
+    visit questions_path
+
+    questions.each do |question|
+      expect(page).to have_content question.title
+      expect(page).to have_content question.body[0..50]
+    end
+  end
+  scenario 'Authenticated user can view all questions' do
+    sign_in(user)
+    visit questions_path
+
+    questions.each do |question|
+      expect(page).to have_content question.title
+      expect(page).to have_content question.body[0..50]
+    end
+  end
+end
