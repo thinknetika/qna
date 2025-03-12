9570abc09089dc303a9067a97ac8773307394349
elbub1979 <ilya.leuh09@gmail.com>
2025-03-12 21:46:14 +0300

    refactoring: question_index partial - move edit link (must visible if user not authenticate); add js: true in features/question/show_spec test

--- spec/features/question/show_spec.rb
+++ spec/features/question/show_spec.rb
@@ -1,16 +1,18 @@
 require 'rails_helper'
 
-feature 'The user can view the question', %q(
+feature 'The user can view the question', js: true do
+  %q(
   To get information or find answers
   As an authenticated or non-authenticated user
   I would like to be able to view a specific question.
-) do
+)
   given(:user) { create(:user) }
   given(:question) { create(:question) }
   given!(:answers) { create_list(:answer, 3, question: question) }
 
   scenario 'Unauthenticated user can view the question' do
     visit questions_path
+
     click_on('Show')
 
     expect(page).to have_content question.title
