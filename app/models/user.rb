d689cd763183194d4e4f8b30db6b507fcb357cd8
elbub1979 <ilya.leuh09@gmail.com>
2025-03-22 23:51:59 +0300

    Revert "changed the margins in question controller; removed the method accessibility check from the user model (ref)"
    This reverts commit 145a5e84fbd568a8489a77a16b1502880aebc6c3.

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,6 @@ class User < ApplicationRecord
   has_many :answers, foreign_key: "author_id", dependent: :destroy
 
   def owns?(resource)
-    id == resource.author_id
+    resource.respond_to?(:author_id) && id == resource.author_id
   end
 end
