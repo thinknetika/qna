f2e0df12085f01e18cb1dacfd8c1b3929fc0f001
elbub1979 <ilya.leuh09@gmail.com>
2025-03-27 22:19:45 +0300

    added boostrap, scss, some styles refactoring; active storage; seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,14 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+user = User.create!(email: 'user1@mail.ru', password: '123456')
+User.create!(email: 'user2@mail.ru', password: '123456')
+
+question = Question.create!(title: 'title question 1', body: 'body question 1', author: user)
+Question.create!(title: 'title question 2', body: 'body question 2', author: user)
+Question.create!(title: 'title question 3', body: 'body question 3', author: user)
+
+Answer.create!(body: 'answer question 1', question: question, author: user)
+Answer.create!(body: 'answer question 2', question: question, author: user)
+Answer.create!(body: 'answer question 3', question: question, author: user)
