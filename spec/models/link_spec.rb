d56fc71599abc5f99988838691b7fb5f1c850317
elbub1979 <ilya.leuh09@gmail.com>
2025-04-14 18:55:00 +0300

    1. added invalid validates in link model test 2. some refactoring assign_reward method in best answers controller

--- spec/models/link_spec.rb
+++ spec/models/link_spec.rb
@@ -2,9 +2,23 @@ require 'rails_helper'
 
 RSpec.describe Link, type: :model do
   describe 'validation' do
-    it { should validate_presence_of :name }
-    it { should validate_presence_of :url }
-    it { should allow_value('https://example.com/').for(:url) }
+    context 'name' do
+      it { should validate_presence_of :name }
+    end
+
+    context 'url' do
+      context 'url presence' do
+        it { should validate_presence_of :url }
+      end
+
+      context 'url valid' do
+        it { should allow_value('https://example.com/').for(:url) }
+      end
+
+      context 'url invalid' do
+        it { should_not allow_value('//example.com/').for(:url) }
+      end
+    end
   end
 
   describe 'association' do
