5820a83bfa328c8c0ba68b50dbaff6f605b42266
elbub1979 <ilya.leuh09@gmail.com>
2025-04-21 17:56:23 +0300

    1. refactoring votable concern 2. refactoring vote model

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -4,4 +4,16 @@ class Vote < ApplicationRecord
 
   validates :value, inclusion: { in: [ -1, 0, 1 ] }
   validates :votable_id, uniqueness: { scope: [ :user_id, :votable_type ] }
+
+  def self.create_or_update_vote(votable, user, value)
+    vote = votable.votes.find_or_initialize_by(user: user)
+
+    if vote.value == value
+      vote.value = 0
+    else
+      vote.value = value
+    end
+
+    vote
+  end
 end
