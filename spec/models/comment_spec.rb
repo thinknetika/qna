17b41072157a5891b2b5f93ef77df690fbd298b1
elbub1979 <ilya.leuh09@gmail.com>
2025-05-10 00:32:09 +0300

    1. refactoring comment model test 2. added validate presence body test in comment model test

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -2,28 +2,34 @@ require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
   describe 'associations' do
-    context 'user' do
-      it { should belong_to :user }
+    context 'author' do
+      it { should belong_to(:author) }
     end
 
     context 'commentable' do
       it { should belong_to :commentable }
     end
-  end
 
-  describe 'commentable - question' do
-    it 'should be associated with a question' do
-      comment = create(:comment, :for_question)
+    context 'commentable - question' do
+      it 'should be associated with a question' do
+        comment = create(:comment, :for_question)
 
-      expect(comment.commentable).to be_a(Question)
+        expect(comment.commentable).to be_a(Question)
+      end
+    end
+
+    context 'commentable - answer' do
+      it 'should be associated with an answer' do
+        comment = create(:comment, :for_answer)
+
+        expect(comment.commentable).to be_a(Answer)
+      end
     end
   end
 
-  describe 'commentable - answer' do
-    it 'should be associated with an answer' do
-      comment = create(:comment, :for_answer)
-
-      expect(comment.commentable).to be_a(Answer)
+  describe "validations" do
+    context "body" do
+      it { should validate_presence_of(:body) }
     end
   end
 end
