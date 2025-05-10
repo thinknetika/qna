4d7a95fa8b349819971771ce7283e015e55250ff
elbub1979 <ilya.leuh09@gmail.com>
2025-05-11 01:41:43 +0300

    1. added functionality to delete the corresponding blocks to an unauthorized user, as:  - added question stimulus controller  - refactoring the corresponding views

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -6,5 +6,8 @@ eagerLoadControllersFrom("controllers", application)
 import AuthorizeController from "./authorize_controller"
 application.register("authorize", AuthorizeController)
 
-import AnswersController from "./answer_controllers/answers_controller"
+import AnswersController from "./answers_controllers/answers_controller"
 application.register("answers", AnswersController)
+
+import QuestionsController from "./questions_controllers/questions_controller"
+application.register("questions", QuestionsController)
