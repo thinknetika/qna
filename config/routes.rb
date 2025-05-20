04dd261b3a87f960c0978f9a1b90f10ddcd98f1b
elbub1979 <ilya.leuh09@gmail.com>
2025-05-20 19:56:54 +0300

    1. refactoring omniauth_callbacks_controller 2. refactoring find_for_oauth 3. added devise registration views

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { omniauth_callbacks: "omniauth_callbacks" }
+  devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
 
   resources :questions do
     resources :answers, except: %i[index show], shallow: true do
