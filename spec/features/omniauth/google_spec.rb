52ad914e2a528e1e7034d4466d9d3ec70b1ce5ea
elbub1979 <ilya.leuh09@gmail.com>
2025-05-25 21:31:38 +0300

    2. added capybara_email gem 2. added features tests for github, google & yandex features tests

---
+++ spec/features/omniauth/google_spec.rb
@@ -0,0 +1,46 @@
+require 'rails_helper'
+
+feature "GoogleOAuth", js: true do
+  background do
+    OmniAuth.config.test_mode = true
+
+    def mock_github_auth(email)
+      OmniAuth.config.mock_auth[:google_oauth2] = OmniAuth::AuthHash.new({
+                                                                    provider: 'google',
+                                                                    uid: '12345678',
+                                                                    info: {
+                                                                      name: 'Google User',
+                                                                      email: email
+                                                                    },
+                                                                    credentials: {
+                                                                      token: 'mock_token',
+                                                                      refresh_token: 'mock_refresh_token',
+                                                                      expires_at: Time.now + 1.week
+                                                                    }
+                                                                  })
+    end
+
+    mock_github_auth('user@gmail.com')
+  end
+
+  scenario 'Signs in via Google' do
+    visit new_user_session_path
+    click_button 'Sign in with Google'
+
+    expect(page).to have_current_path(root_path)
+    expect(page).to have_content('Successfully authenticated from Google_oauth2 account.')
+    expect(page).to have_content('user@gmail.com')
+    expect(page).to have_no_button('Sign in with Google')
+  end
+
+  scenario 'Sign in via Google fails if user cannot be found' do
+    mock_github_auth(email: 'another_user@gmail.com')
+    allow(User).to receive(:find_for_oauth).and_return([nil, nil])
+
+    visit new_user_session_path
+    click_button 'Sign in with Google'
+
+    expect(page).to have_current_path(new_user_session_path)
+    expect(page).to have_content('Something went wrong')
+  end
+end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,6 +5,7 @@ require_relative '../config/environment'
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'support/capybara'
 require 'rspec/rails'
+require 'capybara/email/rspec'
 
 Dir[Rails.root.join('spec', 'support', '**', '*.rb')].sort.each { |f| require f }
 
@@ -24,6 +25,7 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   config.include Devise::Test::ControllerHelpers, type: :controller
   config.render_views = true
+  config.include Capybara::Email::DSL
 
   config.after(:all) do
     FileUtils.rm_rf("#{Rails.root}/tmp/storage")
