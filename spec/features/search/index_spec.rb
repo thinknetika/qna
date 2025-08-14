45112be934907062a088a51c497db033c25a9719
elbub1979 <ilya.leuh09@gmail.com>
2025-08-14 23:01:36 +0300

    some refactoring

--- spec/features/search/index_spec.rb
+++ spec/features/search/index_spec.rb
@@ -1,4 +1,4 @@
-require 'rails_helper'
+require 'sphinx_helper'
 
 feature 'User can search for content', %q(
   To find specific information
@@ -11,61 +11,67 @@ feature 'User can search for content', %q(
   given!(:search_user) { create(:user, email: 'ruby@developer.com') }
 
   background do
-    visit root_path
+    ThinkingSphinx::Test.run do
+      visit root_path
+    end
   end
 
-  scenario 'User can search for questions', js: true do
-    fill_in 'query', with: 'Ruby programming'
-    select 'Questions', from: 'category'
-    click_button 'Search'
+  scenario 'User can search for questions', sphinx: true, js: true do
+    ThinkingSphinx::Test.run do
+      fill_in 'query', with: 'Ruby programming'
+      select 'Questions', from: 'category'
+      click_button 'Search'
 
-    expect(page).to have_content 'Search results for: Ruby programming', wait: 5
-    expect(page).to have_content question.title
+      expect(page).to have_content 'Search results for: Ruby programming', wait: 5
+      expect(page).to have_content question.title
+    end
   end
 
-  scenario 'User can search for answers', js: true do
-    fill_in 'query', with: 'You should use Ruby on Rails framework'
-    select 'Answers', from: 'category'
-    click_button 'Search'
+  scenario 'User can search for answers', sphinx: true, js: true do
+    ThinkingSphinx::Test.run do
+      fill_in 'query', with: 'You should use Ruby on Rails framework'
+      select 'Answers', from: 'category'
+      click_button 'Search'
 
-    save_and_open_screenshot
-
-    expect(page).to have_content 'Search results for: You should use Ruby on Rails framework', wait: 5
-    save_and_open_screenshot
-    expect(page).to have_content answer.body
+      expect(page).to have_content 'Search results for: You should use Ruby on Rails framework', wait: 5
+      expect(page).to have_content answer.body
+    end
   end
 
-  scenario 'User can search for users', js: true do
-    fill_in 'query', with: 'ruby@developer.com'
-    select 'Users', from: 'category'
-    click_button 'Search'
+  scenario 'User can search for users', sphinx: true, js: true do
+    ThinkingSphinx::Test.run do
+      fill_in 'query', with: 'ruby@developer.com'
+      select 'Users', from: 'category'
+      click_button 'Search'
 
-    save_and_open_screenshot
-
-    expect(page).to have_content 'Search results for: ruby@developer.com', wait: 5
-    save_and_open_screenshot
-    expect(page).to have_content search_user.body
+      expect(page).to have_content 'Search results for: ruby@developer.com', wait: 5
+      expect(page).to have_content search_user.email
+    end
   end
 
-  scenario 'User can search in all categories', js: true do
-    fill_in 'query', with: 'Ruby'
-    select 'All', from: 'category'
-    click_button 'Search'
+  scenario 'User can search in all categories', sphinx: true, js: true do
+    ThinkingSphinx::Test.run do
+      fill_in 'query', with: 'Ruby'
+      select 'All', from: 'category'
+      click_button 'Search'
 
-    expect(page).to have_content 'Search results for: Ruby'
-    expect(page).to have_content 'Questions'
-    expect(page).to have_content 'Answers'
-    expect(page).to have_content 'Users'
+      expect(page).to have_content 'Search results for: Ruby'
+      expect(page).to have_content 'Questions'
+      expect(page).to have_content 'Answers'
+      expect(page).to have_content 'Users'
+    end
   end
 
-  scenario 'Gets no results message when nothing found', js: true do
-    visit root_path
+  scenario 'Gets no results message when nothing found', sphinx: true, js: true do
+    ThinkingSphinx::Test.run do
+      visit root_path
 
-    fill_in 'query', with: 'nquery xyz123'
-    select 'Questions', from: 'category'
-    click_button 'Search'
+      fill_in 'query', with: 'nonexistent query xyz123'
+      select 'Questions', from: 'category'
+      click_button 'Search'
 
-    expect(page).to have_content 'Search results for: nonexistent query xyz123'
-    expect(page).to have_content 'No questions found'
+      expect(page).to have_content 'Search results for: nonexistent query xyz123'
+      expect(page).to have_content 'No questions found'
+    end
   end
 end
