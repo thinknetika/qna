7f015c349854712fe01ad077c04b8e9f339ec9d3
elbub1979 <ilya.leuh09@gmail.com>
2025-08-25 22:49:04 +0300

    change scheduled task by delta rake "ts:merge"

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,32 +1,10 @@
-# Use this file to easily define all of your cron jobs.
-#
-# It's helpful, but not entirely necessary to understand cron before proceeding.
-# http://en.wikipedia.org/wiki/Cron
-
-# Example:
-#
-# set :output, "/path/to/my/cron_log.log"
-#
-# every 2.hours do
-#   command "/usr/bin/some_great_command"
-#   runner "MyModel.some_method"
-#   rake "some:great:rake:task"
-# end
-#
-# every 4.days do
-#   runner "AnotherModel.prune_old_records"
-# end
-
-# Learn more: http://github.com/javan/whenever
-
-# daily digest scheduled
-every 1.days do
+every 1.day do
   runner "DailyDigestJob.perform_now"
 end
 
-#thinking sphinx scheduled
+# thinking sphinx scheduled
 every 15.minutes do
-  rake "ts:delta"
+  rake "ts:merge"
 end
 
 every 30.minutes do
